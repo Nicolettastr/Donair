76d64e7f2c0758ff3e74d87d8b3fbde31e09373c
Nicole Struggia <103970504+Nicolettastr@users.noreply.github.com>
2023-05-23 21:35:24 +0200

    contact

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,6 +1,7 @@
 import '../styles/home.css'
 import logo from '../../public/logo_T.png'
 import Card_component from '../components/card';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
 
@@ -15,7 +16,12 @@ const Home = () => {
             </div>
         </section>
         <section className='home_infoSection d_center'>
-            <h4>Somos Donair, tu aliado en servicios, climatización y   Proyectos. Confía en Donair y disfruta de resultados impecables.</h4>
+            <h4>
+                Somos Donair, tu aliado en servicios, climatización y   Proyectos. Confía en Donair y disfruta de resultados impecables.
+            </h4>
+            <Link to='/contact'>
+                <button className='home_contactButton contact'>Contáctanos</button>
+            </Link>
             <div className='home_cardSection d_center'>
                 <Card_component/>
             </div>
@@ -31,6 +37,16 @@ const Home = () => {
                     <video className='home_video' src="/DonAir-1.mov" controls></video>
                     <video className='home_video' src="/DonAir-2.mov" controls></video>
                 </div>
+            </div>
+        </section>
+        <section className='home_contactBanner d_center'>
+            <div className='home_contactSection'>
+                <h4>
+                    Servicios especializados que transforman tus espacios. ¿Tienes un proyecto en mente? Hazlo realidad con nosotros! 
+                </h4>
+                <Link to='/contact'>
+                    <button className='home_contactButton'>Contáctanos</button>
+                </Link>
             </div>
         </section>
         </>
