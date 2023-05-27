8d5b51547294c56defa031a008a5aa2cb79acdf0
Nicole Struggia <103970504+Nicolettastr@users.noreply.github.com>
2023-05-27 15:11:28 +0200

    services fixed

--- src/pages/servicios.jsx
+++ src/pages/servicios.jsx
@@ -5,15 +5,66 @@ import serviceIcon from '../../public/tecnico.png'
 import climatizacionIcon from '../../public/casa.png'
 import proyectosIcon from '../../public/proyecto.png'
 
+import Fontanería from '../../public/servicesIcon/valvula.png'
+import Carpintería from '../../public/servicesIcon/carpinteria.png'
+import Cristalería from '../../public/servicesIcon/vaso.png'
+import Electricidad from '../../public/servicesIcon/enchufe.png'
+import Albañilería from '../../public/servicesIcon/albanil.png'
+import Limpieza from '../../public/servicesIcon/limpiar.png'
+import Pintura from '../../public/servicesIcon/pintura.png'
+
+import aire from '../../public/climatizacionIcon/aire.png'
+import filtro from '../../public/climatizacionIcon/filtro.png'
+import humo from '../../public/climatizacionIcon/humo.png'
+import instalar from '../../public/climatizacionIcon/instalar.png'
+import mantenimiento from '../../public/climatizacionIcon/mantenimiento.png'
+import refrigerar from '../../public/climatizacionIcon/refrigerar.png'
+import ventilacion from '../../public/climatizacionIcon/ventilacion.png'
+climatizacionIcon
+
+import tienda from '../../public/proyectosIcon/tienda.png'
+import local from '../../public/proyectosIcon/local.png'
+import piscina from '../../public/proyectosIcon/piscina.png'
+import terraza from '../../public/proyectosIcon/terraza.png'
+import bombilla from '../../public/proyectosIcon/bombilla.png'
+
+
 const Servicios = () => {
 
   const allServices = [
     {
       name: 'Servicios',
-      text: 'Mantenimiento integral de edificio y viviendas:', 
+      text: 'Mantenimiento integral de edificios y viviendas:', 
       class: 'services',
       services: [
-        'Fontanería', 'Carpintería', 'Cristalería', 'Electricidad', 'Limpieza', 'Pintura', 'Albañilería'
+        {
+          name: "Fontanería",
+          icon: Fontanería,
+        },
+        {
+          name: "Carpintería",
+          icon: Carpintería,
+        },
+        {
+          name: "Cristalería",
+          icon: Cristalería,
+        },
+        {
+          name: "Electricidad",
+          icon: Electricidad,
+        },
+        {
+          name: "Limpieza",
+          icon: Limpieza,
+        },
+        {
+          name: "Pintura",
+          icon: Pintura,
+        },
+        {
+          name: "Albañilería",
+          icon: Albañilería,
+        }
       ],
       icon: serviceIcon
     },
@@ -22,7 +73,34 @@ const Servicios = () => {
       name: 'Climatización',
       class: 'climatizacion',
       services: [
-        'Venta de equipos', 'Montaje, instalación', 'Servicio técnico', 'Mantenimiento', 'Extracción de aire', 'Ventilación de aire', 'Instalación de salidas de humo', 'Instalación chimeneas', 'Mantenimiento de chimeneas', 'Unidades de tratamiento de aire', 'Sistemas de refrigeración'
+        {
+          name:'Venta, Montaje e instalación de equipos ',
+          icon: aire,
+        },
+        {
+          name:'Servicio técnico y Mantenimiento',
+          icon: mantenimiento,
+        },
+        {
+          name:'Extracción y Ventilación de aire',
+          icon: ventilacion,
+        },
+        {
+          name:'Instalación de salidas de humo',
+          icon: humo,
+        },
+        {
+          name:'Instalación y Mantenimiento chimeneas',
+          icon: instalar
+        },
+        {
+          name:'Unidades de tratamiento de aire',
+          icon: filtro
+        },
+        {
+          name: 'Sistemas de refrigeración',
+          icon: refrigerar,
+        }
       ],
       icon: climatizacionIcon
     },
@@ -31,7 +109,26 @@ const Servicios = () => {
       textEnd: 'Confía en nosotros para hacer realidad tus ideas. Nuestro equipo de profesionales se encargará de cada detalle para lograr resultados excepcionales.',
       class: 'proyectos',
       services: [
-        'Piscinas', 'Tiendas','Terrazas','Locales'
+        {
+          name: "Piscinas",
+          icon: piscina,
+        },
+        {
+          name: "Tiendas",
+          icon: tienda,
+        },
+        {
+          name: "Terrazas",
+          icon: terraza,
+        },
+        {
+          name: "Locales",
+          icon: local,
+        },
+        {
+          name: "Tu Idea",
+          icon: bombilla,
+        }
       ],
       icon: proyectosIcon
     }
@@ -41,18 +138,20 @@ const Servicios = () => {
     return (
       <section key={index} className={`${item.class}_section services_pad`} id={item.class}>
       <div className={`${item.class}_container`}>
-        
         <h2 className={`${item.class}_title type_title`}>
-        <img className='service_icon' src={item.icon} alt={`${item.name} icon`} />
+        <img className='service_icon service_iconTitle' src={item.icon} alt={`${item.name} icon`} />
         {item.name}
         </h2>
         <div>
           <ul className={`services_list ${item.class}_list`}>
-            <p>{item.text}</p>
             {item.services.map((item, index) => {
-              return <li className='services_itemList' key={index}>{item}</li>
+              return <li className='services_itemList' key={index}>
+                <span>
+                  <img className='service_icon' src={item.icon} alt="" />
+                </span>
+                {item.name}
+                </li>
             })}
-            {item.textEnd ? <p className='project_p'>{item.textEnd}</p> : ''}
           </ul>
         </div>
       </div>
@@ -64,7 +163,7 @@ const Servicios = () => {
     <div className='pag_container services_sectionContainer d_center'>
         <div className='d_center services_sectionAll'>
         <ServicesBanner/>
-        <div className='services_mainContainer'>
+        <div className='services_mainContainer d_center'>
           {works}
         </div>
           <Work/>
