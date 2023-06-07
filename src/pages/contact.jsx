9ae1621604b94ac40170dca30275409336cb204b
Nicole Struggia <103970504+Nicolettastr@users.noreply.github.com>
2023-06-07 19:36:42 +0200

    contact

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,24 +1,47 @@
-import { useContext } from 'react'
+import { useContext, useState } from 'react'
 import '../styles/contact.css'
 import {Context} from '../store/appContext'
+import { useNavigate } from 'react-router-dom'
 
 const Contact = () => {
 
   const {store} = useContext(Context)
+  const navigate = useNavigate()
 
   const contactInputs = ['nombre', 'email', 'asunto', 'móvil']
+  const [isFormValid, setIsFormValid] = useState(false);
+
+  const handleNavigate = () => {
+    if (isFormValid) {
+      navigate('/thanks');
+    }
+  };
+
+  const handleInputChange = () => {
+    const formFields = document.querySelectorAll('.contact-form-field');
+    let isValid = true;
+  
+    formFields.forEach((field) => {
+      if (field.value.trim() === '') {
+        isValid = false;
+      }
+    });
+  
+    setIsFormValid(isValid);
+  };
 
   const contact = contactInputs.map((item, index) => {
     return (
       <div className='contact_inputContainer' key={index}>
         <label htmlFor={item}>{item.replace(/^\w/, (c) => c.toUpperCase())}</label>
-            <input
-              className="contact-input"
-              type={item.includes('email') ? 'email' : 'text'}
-              name={item}
-              placeholder={item.replace(/^\w/, (c) => c.toUpperCase())}
-              required
-            />
+          <input
+            className="contact-input contact-form-field"
+            type={item.includes('email') ? 'email' : 'text'}
+            name={item}
+            placeholder={item.replace(/^\w/, (c) => c.toUpperCase())}
+            required
+            onChange={handleInputChange}
+          />
       </div>
     )
   })
@@ -53,14 +76,15 @@ const Contact = () => {
                   name="text"
                   placeholder="Mensaje"
                   required
+                  onChange={handleInputChange}
                 ></textarea>
               </div>
-            <input type="hidden" name="_next" value="https://donair.es/thanks"></input>
             </div>
         </div>
         
         <div className='contact_btnContainer'>
         <button
+              onClick={handleNavigate}
               type="submit"
               className="contact_btn"
             >
